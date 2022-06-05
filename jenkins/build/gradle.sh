7bde7e5cadbef3d7c9bf0fcb2ae8602427c4b124
jeidiiy <wldyd2356@gmail.com>
2022-06-05 18:38:28 +0900

    fix: PWD 확인

--- jenkins/build/gradle.sh
+++ jenkins/build/gradle.sh
@@ -4,6 +4,7 @@ echo "**********************"
 echo "**** Building jar ****"
 echo "**********************"
 
+echo $PWD
 export JAVA_IMAGE=openjdk:11
 
 docker run -d --rm -v $PWD:/app -w /app $JAVA_IMAGE "$@"
