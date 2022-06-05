72b921f7b3ecce68b6c73c96bfefca92d5334fa8
jeidiiy <wldyd2356@gmail.com>
2022-06-05 19:35:19 +0900

    fix: build tag

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -4,4 +4,6 @@ echo "*******************************"
 echo "**** Building Docker Image ****"
 echo "*******************************"
 
+export $BUILD_TAG=1
+
 cd $PWD/jenkins/build/ && docker compose -f docker-compose-build.yml build
