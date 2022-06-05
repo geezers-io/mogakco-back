9999f6086f4316f11d737fc8dfdf7bb66f9d5695
jeidiiy <wldyd2356@gmail.com>
2022-06-05 19:47:50 +0900

    fix: build tag $BUILD_NUMBER로 변경

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -4,6 +4,4 @@ echo "*******************************"
 echo "**** Building Docker Image ****"
 echo "*******************************"
 
-export BUILD_TAG=1
-
 cd $PWD/jenkins/build/ && docker compose -f docker-compose-build.yml build
--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -10,7 +10,7 @@ echo "** Logging in Docker hub ***"
 docker login -u jeidiiy -p $PASSWORD
 
 echo "****** Tagging image *******"
-docker tag $IMAGE:$BUILD_TAG jeidiiy/$IMAGE:$BUILD_TAG
+docker tag $IMAGE:$BUILD_NUMBER jeidiiy/$IMAGE:$BUILD_NUMBER
 
 echo "****** Pushing image *******"
-docker push jeidiiy/$IMAGE:$BUILD_TAG
+docker push jeidiiy/$IMAGE:$BUILD_NUMBER
