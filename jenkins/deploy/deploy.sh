5025b4cc36efe2e3624c70708af25349a84b23e8
jeidiiy <wldyd2356@gmail.com>
2022-06-05 19:54:43 +0900

    fix: build tag 수정

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,7 +5,7 @@ echo "***** Deploying ******"
 echo "**********************"
 
 echo mogakco-back >/tmp/.auth
-echo $BUILD_TAG >>/tmp/.auth
+echo $BUILD_NUMBER >>/tmp/.auth
 
 scp -i ~/.ssh/id_rsa /tmp/.auth ubuntu@ec2-3-36-75-245.ap-northeast-2.compute.amazonaws.com:/tmp/.auth
 
