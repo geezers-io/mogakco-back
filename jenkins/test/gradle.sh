63e21838173e021700d31d3c448e618a31dad567
jeidiiy <wldyd2356@gmail.com>
2022-06-05 19:27:31 +0900

    fix: gradle test 변경

--- jenkins/test/gradle.sh
+++ jenkins/test/gradle.sh
@@ -4,4 +4,4 @@ echo "**********************"
 echo "****** Testing *******"
 echo "**********************"
 
-docker run -d --rm -v $PWD/mogakco-back:/app -w /app $JAVA_IMAGE "$@"
+docker run -d --rm -v $PWD/mogakco-back:/app -w /app $GRADLE_IMAGE "$@"
