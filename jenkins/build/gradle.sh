dd9ce7a9212aabd2646a44c7faff40107516ce7d
jeidiiy <wldyd2356@gmail.com>
2022-06-05 21:43:24 +0900

    fix: 빌드 소스 경로 수정

--- jenkins/build/gradle.sh
+++ jenkins/build/gradle.sh
@@ -6,4 +6,4 @@ echo "**********************"
 
 export GRADLE_IMAGE="gradle:7.4.2-jdk11-alpine"
 
-docker run -d --rm -v $PWD/mogakco-back:/app -w /app $GRADLE_IMAGE "$@"
+docker run -d --rm -v $PWD:/app -w /app $GRADLE_IMAGE "$@"
