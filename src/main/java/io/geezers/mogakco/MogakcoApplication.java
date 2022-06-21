27cfce02e931b0b976fd0d8d1eea460b797f6a71
Ji-Yong, Jeong <jeidiiy98@gmail.com>
2022-06-21 23:59:11 +0900

    Apply session management with embedded redis (#5)
    - 임베디드 redis 서버는 local 환경에 한정됨
    - 향후 실제 서비스에선 별도의 AWS 인스턴스를 활용할 것

--- src/main/java/io/geezers/mogakco/MogakcoApplication.java
+++ src/main/java/io/geezers/mogakco/MogakcoApplication.java
@@ -3,7 +3,10 @@ package io.geezers.mogakco;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+
+@EnableRedisHttpSession
 @EnableWebSecurity
 @SpringBootApplication
 public class MogakcoApplication {
