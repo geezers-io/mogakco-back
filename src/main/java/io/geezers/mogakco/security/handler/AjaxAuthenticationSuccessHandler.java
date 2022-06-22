1cef939c3259dbdce87bc669dd179094d2cf3479
Ji-Yong, Jeong <jeidiiy98@gmail.com>
2022-06-22 19:04:55 +0900

    Inject ObjectMapper bean instead of local variable

--- src/main/java/io/geezers/mogakco/security/handler/AjaxAuthenticationSuccessHandler.java
+++ src/main/java/io/geezers/mogakco/security/handler/AjaxAuthenticationSuccessHandler.java
@@ -1,9 +1,10 @@
 package io.geezers.mogakco.security.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.geezers.mogakco.domain.dto.user.UserLoginResponseDto;
-import io.geezers.mogakco.domain.dto.user.UserAuthenticationDto;
 import io.geezers.mogakco.domain.dto.user.Me;
+import io.geezers.mogakco.domain.dto.user.UserAuthenticationDto;
+import io.geezers.mogakco.domain.dto.user.UserLoginResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -12,11 +13,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@RequiredArgsConstructor
 public class AjaxAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
         response.setStatus(HttpServletResponse.SC_OK);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 
