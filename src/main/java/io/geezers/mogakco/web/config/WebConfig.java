8094239c434aee3cf073a28a2297b5fd3261c1be
Ji-Yong, Jeong <jeidiiy98@gmail.com>
2022-06-24 00:47:44 +0900

    Add Weblog filter (MK-43) (#10)
    * Add Weblog filter
    
    - 이 필터는 보안 관련 필터가 아니므로 Spring Security의 filter chain에 넣을 필요가 없다고 판단했다. 그래서 springSecurityfilterChain의 앞에 위치하게 하여 요청과 응답에 관한 로깅을 하도록 했다.
    - springSecurityFilterChain의 default order는 -100이다.
    - OrderedRequestContextFilter의 default order는 -105이다.
    - 따라서 그 중간에 끼우기 위해 order를 -104로 지정했다.
    
    * Move multiply number's position

---
+++ src/main/java/io/geezers/mogakco/web/config/WebConfig.java
@@ -0,0 +1,24 @@
+package io.geezers.mogakco.web.config;
+
+import io.geezers.mogakco.api.common.SimpleWeblogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean<Filter> filterRegistrationBean() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+
+        filterRegistrationBean.setFilter(new SimpleWeblogFilter());
+        filterRegistrationBean.setOrder(-104);
+        filterRegistrationBean.addUrlPatterns("/api/*");
+
+        return filterRegistrationBean;
+    }
+}
